f404d92fd020efa47547313118a91622c19bd536
Alexander Drichel <alexander@drichel.com>
2024-03-10 15:38:24 +0100

    build: project setup
    installed vue, tailwind and configured eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,37 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution');
+
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-airbnb-with-typescript',
+  ],
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{cy,spec}.{js,ts,jsx,tsx}',
+        'cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}',
+        'cypress/support/**/*.{js,ts,jsx,tsx}',
+      ],
+      extends: [
+        'plugin:cypress/recommended',
+      ],
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'vue/max-len': ['warn', {
+      code: 100,
+      ignoreStrings: true,
+      ignoreUrls: true,
+      ignoreTemplateLiterals: true,
+      ignoreHTMLAttributeValues: true,
+      ignoreComments: true,
+    }],
+  },
+};
---
+++ README.md
@@ -0,0 +1,4 @@
+# envirobob ui 
+
+This is a small side project to build a slick ui that displays data coming from environmental measurement devices
+starting with a Geiger counter.
